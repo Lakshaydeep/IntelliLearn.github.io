f20dde6653bac978c0f328299a5c572368f0c8c0
aaditay <aaditaygargktl@gmail.com>
2020-06-04 21:21:08 +0530

    functionalities added for Kinematics
    + when velocity is too much we handled that

--- Apps/Workspace/Kinematics/animate_ball.js
+++ Apps/Workspace/Kinematics/animate_ball.js
@@ -6,32 +6,44 @@ let accInp = document.querySelector("#acc-inp");
 let velocityParam = document.querySelector("#velocity-param");
 let accParam = document.querySelector("#acc-param");
 
-action.onclick = do_experiment();
+let scaleInp = document.querySelector("#scale").querySelector("input");
+
+action.onclick = do_experiment;
 
 function do_experiment() {
+  let width = 50;
   let velocity = velocityInp.value;
   let acc = accInp.value;
+  let scale = scaleInp.value;
+
+  //------- Default Values for parameters ------
   if (velocity == "") {
     velocity = 1;
   }
   if (acc == "") {
-    acc = 9.8;
+    acc = -9.8;
   }
-  velocity = (velocity * 10) / 1000; // Velocity in px/ms
-  acc = (acc * 10) / 1000000; // Acceleration in px/ms*2
+  if (scale == "") {
+    scale = 10;
+  }
+
+  //---------------------------------------------
+
+  velocity = (velocity * scale) / 1000; // Velocity in px/ms
+  acc = (acc * scale) / 1000000; // Acceleration in px/ms*2
 
   let goUp = setInterval(moveUp, 1);
   let pos = 400;
   function moveUp() {
-    velocity = velocity - acc;
-    console.log(velocity);
-
+    velocity = velocity + acc;
     pos = pos - velocity;
-    ball.style.top = String(pos) + "px";
+    if (pos < 5) {
+      width = width - velocity / 100;
 
-    if (velocity == 0) {
-      console.log("STOPPED ");
-      clearInterval(goUp);
+      ball.style.width = String(width) + "px";
+      ball.style.height = String(width) + "px";
+    } else {
+      ball.style.top = String(pos) + "px";
     }
   }
 }
