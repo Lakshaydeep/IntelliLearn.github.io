be77f5b5d266d33c61ded34561abe03dc8ed770b
aaditay <aaditaygargktl@gmail.com>
2020-06-04 12:56:59 +0530

    Added DotPhy and UI changes
    +kinematics section  in DOTPHY
    +UI in index made responsive
    + News on index

---
+++ Apps/Workspace/animate_ball.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("button");
+let ball = document.querySelector("#ball");
+let velocity = document.querySelector("#velocity-inp");
+
+button.onclick = function () {
+  /*  let speed = velocity.value;
+  if (speed == "") {
+    speed = 1;
+  }
+  speed = speed * 10;
+  let distance = (speed * speed) / 200;
+  console.log(distance);*/
+};
